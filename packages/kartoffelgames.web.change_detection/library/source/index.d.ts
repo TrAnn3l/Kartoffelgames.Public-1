d2d0b496bf4e22df32693bfbd90c3f458e128dc8
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2022-02-19 03:32:33 +0100

    Untrack files.

--- packages/kartoffelgames.web.change_detection/library/source/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { CompareHandler } from './comparison/compare-handler';
-export { ChangeDetection, ChangeDetectionReason } from './change_detection/change-detection';
-export { DifferenceSearch, HistoryItem, ChangeState } from './comparison/difference-search';
-//# sourceMappingURL=index.d.ts.map
