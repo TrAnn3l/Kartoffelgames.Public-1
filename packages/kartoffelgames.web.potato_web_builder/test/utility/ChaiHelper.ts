1a98e7bfd976338362927d8e7227b0dd9b708a0a
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2022-02-23 01:10:10 +0100

    Better chai component structure compare.

--- packages/kartoffelgames.web.potato_web_builder/test/utility/ChaiHelper.ts
+++ packages/kartoffelgames.web.potato_web_builder/test/utility/ChaiHelper.ts
@@ -3,65 +3,93 @@ import { Assertion } from 'chai';
 
 
 Assertion.addMethod('componentStructure', function (pChilds: ComponentStructure, pUseShadowRoot: boolean) {
-    const lCreateMap = (pComponent: Element, pExpectedChilds: ComponentStructure, pUseShadowRoot: boolean): ComponentStructure => {
-        const lChildList = (pUseShadowRoot) ? pComponent.shadowRoot.childNodes : pComponent.childNodes;
-        const lActualStucture: ComponentStructure = new Array<InjectionConstructor | ChildNodeStructure>();
+    const lRecreateElementStructure = <T extends InjectionConstructor | ChildNodeStructure>(pExpectedStructure: T, pActualNode: Node): T => {
+        // Check if structure or constructor.
+        if (typeof pExpectedStructure === 'function') {
+            return <T><InjectionConstructor>pActualNode.constructor;
+        } else {
+            // Structure.
+            const lExpectedStructure: ChildNodeStructure = pExpectedStructure;
 
-        // Check each index of child node.
-        for (let lIndex: number = 0; lIndex < lChildList.length || lIndex < pExpectedChilds.length; lIndex++) {
-            // Get actual class.
-            let lActualClass: InjectionConstructor;
-            let lActualNode: Node;
-            if (lIndex < lChildList.length) {
-                lActualClass = <InjectionConstructor>lChildList[lIndex].constructor;
-                lActualNode = lChildList[lIndex];
-            } else {
-                lActualClass = null;
-                lActualNode = null;
-            }
+            // Create new structure.
+            const lNewStructure: ChildNodeStructure = {
+                node: <InjectionConstructor>pActualNode.constructor
+            };
 
-            // Get expected child class and strcture.
-            let lExpectedClass: InjectionConstructor;
-            let lExpectedStructure: ChildNodeStructure;
-            if (lIndex < pExpectedChilds.length) {
-                if (typeof pExpectedChilds[lIndex] === 'function') {
-                    lExpectedClass = <InjectionConstructor>pExpectedChilds[lIndex];
-                    lExpectedStructure = null;
-                } else {
-                    lExpectedClass = (<ChildNodeStructure>pExpectedChilds[lIndex]).node;
-                    lExpectedStructure = <ChildNodeStructure>pExpectedChilds[lIndex];
-                }
-            } else {
-                lExpectedClass = null;
-                lExpectedStructure = null;
-            }
-
-            if (lExpectedClass === null) {
-                lActualStucture.push(null);
-            } else if (lExpectedStructure) {
-                if (lActualNode instanceof Element) {
-                    const lStructure: ChildNodeStructure = {
-                        node: lActualClass,
-                        childs: lCreateMap(lActualNode, lExpectedStructure.childs, lExpectedStructure.useShadowRoot)
-                    };
-
-                    if ('useShadowRoot' in lExpectedStructure) {
-                        lStructure.useShadowRoot = lExpectedStructure.useShadowRoot;
+            // Recreate child list.
+            if ('childs' in lExpectedStructure) {
+                if (pActualNode instanceof Element) {
+                    if (lExpectedStructure.useShadowRoot) {
+                        lNewStructure.childs = lRecreateComponentChildStructure(lExpectedStructure.childs, pActualNode.shadowRoot);
+                    } else {
+                        lNewStructure.childs = lRecreateComponentChildStructure(lExpectedStructure.childs, pActualNode);
                     }
-
-                    lActualStucture.push(lStructure);
                 } else {
-                    lActualStucture.push(lActualClass);
+                    lNewStructure.childs = null;
                 }
+            }
+
+            // Copy "useShadowRoot".
+            if ('useShadowRoot' in lExpectedStructure) {
+                lNewStructure.useShadowRoot = lExpectedStructure.useShadowRoot;
+            }
+
+            // Recreate attributes.
+            if ('attributes' in lExpectedStructure) {
+                if (pActualNode instanceof Element) {
+                    // Create empty attribute list.
+                    lNewStructure.attributes = new Array<{ name: string, value: string; }>();
+
+                    // Try to add all expected attributes.
+                    for (const lAttribute of lExpectedStructure.attributes) {
+                        if (pActualNode.hasAttribute(lAttribute.name)) {
+                            lNewStructure.attributes.push({
+                                name: lAttribute.name,
+                                value: pActualNode.getAttribute(lAttribute.name)
+                            });
+                        } else {
+                            lNewStructure.attributes.push(null);
+                        }
+                    }
+                } else {
+                    lNewStructure.attributes = null;
+                }
+            }
+
+            return <T>lNewStructure;
+        }
+    };
+
+    const lRecreateComponentChildStructure = (pExpectedStructure: ComponentStructure, pActualElement: Element | ShadowRoot): ComponentStructure => {
+        const lRecreatedStructureList: ComponentStructure = new Array<InjectionConstructor | ChildNodeStructure>();
+
+        // Get max length of expected or actual childs.
+        const lMaxChildCount: number = Math.max(pExpectedStructure.length, pActualElement.childNodes.length);
+
+        // For each possible child.
+        for (let lIndex: number = 0; lIndex < lMaxChildCount; lIndex++) {
+            const lExpectedChild: InjectionConstructor | ChildNodeStructure = pExpectedStructure[lIndex] ?? null;
+            const lActualChild: Node = pActualElement.childNodes[lIndex] ?? null;
+
+            // Kepp null values.
+            if (lExpectedChild === null || lActualChild === null) {
+                lRecreatedStructureList.push(null);
             } else {
-                lActualStucture.push(lActualClass);
+                // Try to recreate component structure.
+                lRecreatedStructureList.push(lRecreateElementStructure(lExpectedChild, lActualChild));
             }
         }
 
-        return lActualStucture;
+        return lRecreatedStructureList;
     };
 
-    const lActualStructure: ComponentStructure = lCreateMap(this._obj, pChilds, pUseShadowRoot);
+    let lActualStructure: ComponentStructure;
+    if (pUseShadowRoot) {
+        lActualStructure = lRecreateComponentChildStructure(pChilds, (<Element>this._obj).shadowRoot);
+    } else {
+        lActualStructure = lRecreateComponentChildStructure(pChilds, <Element>this._obj);
+    }
+
     new Assertion(pChilds).to.be.deep.equal(lActualStructure);
 });
 
@@ -70,7 +98,8 @@ export type ComponentStructure = Array<InjectionConstructor | ChildNodeStructure
 export type ChildNodeStructure = {
     node: InjectionConstructor,
     useShadowRoot?: boolean;
-    childs: ComponentStructure;
+    childs?: ComponentStructure;
+    attributes?: Array<{ name: string, value: string; }>;
 };
 
 declare global {
