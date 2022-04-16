7cc60a3d2d2e8f1cebe064166d04e9c214eb189a
TetrodoPotato <TetrodoToxinPotato@gmail.com>
2022-04-16 07:01:59 +0200

    Fix default splash screen.

--- packages/kartoffelgames.web.potato_web_builder/source/pwb-app.ts
+++ packages/kartoffelgames.web.potato_web_builder/source/pwb-app.ts
@@ -78,8 +78,8 @@ export class PwbApp {
 
         // Set default splash screen.
         this.setSplashScreen({
-            background: 'linear-gradient(0deg, rgba(47,67,254,1) 8%, rgba(0,23,255,1) 70%);',
-            content: '<span style="color: #fff;">PWB</span>',
+            background: 'linear-gradient(0deg, rgba(47,67,254,1) 8%, rgba(0,23,255,1) 70%)',
+            content: '<span style="color: #fff; font-family: arial; font-weight: bold;">PWB</span>',
             animationTime: 500
         });
     }
